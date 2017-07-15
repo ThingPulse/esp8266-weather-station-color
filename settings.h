a835f2f45ed3d76ed12b041e851536af78f6b3e2
Daniel Eichhorn <dani.eichhorn@squix.ch>
2017-07-15 08:46:39 +0200

    Removed personal API key

--- settings.h
+++ settings.h
@@ -40,7 +40,7 @@ const int UPDATE_INTERVAL_SECS = 10 * 60; // Update every 10 minutes
 // e.g. http://api.wunderground.com/api/808ba87ed77c4511/conditions/q/CA/SAN_FRANCISCO.json <- note that in the US you use the state instead of country code
 
 const String DISPLAYED_CITY_NAME = "Zürich";
-const String WUNDERGRROUND_API_KEY = "808ba87ed77c4501";
+const String WUNDERGRROUND_API_KEY = "<WUNDERGROUND KEY>";
 const String WUNDERGRROUND_LANGUAGE = "EN";
 const String WUNDERGROUND_COUNTRY = "CH";
 const String WUNDERGROUND_CITY = "Zurich";
