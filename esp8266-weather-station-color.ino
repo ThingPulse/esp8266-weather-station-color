82863e760c04c2036d74209f53b4e2f395a6d409
lorol <lorolouis@gmail.com>
2017-09-30 11:42:17 -0400

    Fix of 00:00:00 clock after forecasts update.

--- esp8266-weather-station-color.ino
+++ esp8266-weather-station-color.ino
@@ -304,36 +304,39 @@ void drawTime() {
   char time_str[11];
   time_t now = dstAdjusted.time(&dstAbbrev);
   struct tm * timeinfo = localtime (&now);
+  
+  if (timeinfo->tm_year > 116) { // valid, if the year is > 2016
+	  
+	  gfx.setTextAlignment(TEXT_ALIGN_CENTER);
+	  gfx.setFont(ArialRoundedMTBold_14);
+	  gfx.setColor(MINI_WHITE);
+	  String date = ctime(&now);
+	  date = date.substring(0,11) + String(1900 + timeinfo->tm_year);
+	  gfx.drawString(120, 6, date);
 
-  gfx.setTextAlignment(TEXT_ALIGN_CENTER);
-  gfx.setFont(ArialRoundedMTBold_14);
-  gfx.setColor(MINI_WHITE);
-  String date = ctime(&now);
-  date = date.substring(0,11) + String(1900 + timeinfo->tm_year);
-  gfx.drawString(120, 6, date);
+	  gfx.setFont(ArialRoundedMTBold_36);
 
-  gfx.setFont(ArialRoundedMTBold_36);
+	  if (IS_STYLE_12HR) {
+		int hour = (timeinfo->tm_hour+11)%12+1;  // take care of noon and midnight
+		sprintf(time_str, "%2d:%02d:%02d\n",hour, timeinfo->tm_min, timeinfo->tm_sec);
+		gfx.drawString(120, 20, time_str);
+	  } else {
+		sprintf(time_str, "%02d:%02d:%02d\n",timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+		gfx.drawString(120, 20, time_str);
+	  }
 
-  if (IS_STYLE_12HR) {
-    int hour = (timeinfo->tm_hour+11)%12+1;  // take care of noon and midnight
-    sprintf(time_str, "%2d:%02d:%02d\n",hour, timeinfo->tm_min, timeinfo->tm_sec);
-    gfx.drawString(120, 20, time_str);
-  } else {
-    sprintf(time_str, "%02d:%02d:%02d\n",timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
-    gfx.drawString(120, 20, time_str);
+	  gfx.setTextAlignment(TEXT_ALIGN_LEFT);
+	  gfx.setFont(ArialMT_Plain_10);
+	  gfx.setColor(MINI_BLUE);
+	  if (IS_STYLE_12HR) {
+		sprintf(time_str, "%s\n%s", dstAbbrev, timeinfo->tm_hour>=12?"PM":"AM");
+		gfx.drawString(195, 27, time_str);
+	  } else {
+		sprintf(time_str, "%s", dstAbbrev);
+		gfx.drawString(195, 27, time_str);  // Known bug: Cuts off 4th character of timezone abbreviation
+	  }
+	  
   }
-
-  gfx.setTextAlignment(TEXT_ALIGN_LEFT);
-  gfx.setFont(ArialMT_Plain_10);
-  gfx.setColor(MINI_BLUE);
-  if (IS_STYLE_12HR) {
-    sprintf(time_str, "%s\n%s", dstAbbrev, timeinfo->tm_hour>=12?"PM":"AM");
-    gfx.drawString(195, 27, time_str);
-  } else {
-    sprintf(time_str, "%s", dstAbbrev);
-    gfx.drawString(195, 27, time_str);  // Known bug: Cuts off 4th character of timezone abbreviation
-  }
-
   drawWifiQuality();
 
 }
